e3ea83655eb27104c85e23ad0d8c408787f55c69
Nevyana Momchilova <nevyana.dm@gmail.com>
2021-11-20 18:10:38 +0200

    async await

--- fetch.js
+++ fetch.js
@@ -1,8 +1,26 @@
-fetch("./todos.json").then(response => {
-    console.log(response);
-    return response.json();
-}).then(data => {
-    console.log(data);
-}).catch(err => {
-    console.log("rejected", err);
-});
+// async await
+
+const getTodos = async () => {
+    const response = await fetch("./todos.json");
+
+    if(response.status !== 200) {
+        throw new Error("cannot fecth the data");
+    }
+
+    const data = await response.json();
+    
+    return data;
+}
+
+getTodos()
+    .then(data => console.log("resolved: ", data))
+    .catch(err => console.log("rejected: ", err.message));
+
+// fetch("./todos.json").then(response => {
+//     console.log(response);
+//     return response.json();
+// }).then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log("rejected", err);
+// });
